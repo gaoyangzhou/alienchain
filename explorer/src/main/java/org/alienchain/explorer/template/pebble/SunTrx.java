cb5ed4f4e0970b6c47604ee28068bdce4326d1b8
shujiaying1111 <499098820@qq.com>
2019-05-17 11:03:29 +0800

    add SunTrx
    add SunTrx

---
+++ explorer/src/main/java/org/alienchain/explorer/template/pebble/SunTrx.java
@@ -0,0 +1,49 @@
+package org.alienchain.explorer.template.pebble;
+
+import com.mitchellbosecke.pebble.extension.Filter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class SunTrx implements Filter{
+
+	private final static long TRX= 1000000l;
+	
+	@Override
+	public List<String> getArgumentNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object apply(Object input, Map<String, Object> args) {
+		
+		
+
+		NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+		nf.setMaximumFractionDigits(7);
+		
+		if (input instanceof Long ) {
+			
+			return nf.format(new BigDecimal((Long)input).divide(new BigDecimal(TRX)));
+			
+		}else {
+
+			String amount = (String) input;
+			if (StringUtils.isNotBlank(amount)) {
+				
+				
+				return nf.format(new BigDecimal(amount).divide(new BigDecimal(TRX)));
+			}
+			
+		}
+
+			return ""; 
+		
+	}
+
+}
